91d026af405b4181e485c261737cddc22c22fad6
Svyatoslav Oprysko <opryskosv@gmail.com>
2018-10-27 18:30:56 +0300

    Added new components and css rules for them.

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TodoListItem from './TodoListItem';
+import "./TodoList.css";
+
+const TodoList = ({data}) => {
+
+    const elements = data.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps}/>
+            </li>
+        );
+    });
+
+    return (
+        <ul className="list-group todo-list">
+            {elements}
+        </ul>
+    );
+};
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import SearchPanel from './components/SearchPanel';
+import TodoList from './components/TodoList';
+import ItemStatusFilter from './components/ItemStatusFilter';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const data = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Make Awesome App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ];
+
+    return (
+        <div className="todo-app">
+            <Header toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
+            <TodoList data={data} />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
