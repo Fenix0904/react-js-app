bdae8ab7ad82125e1e41c1c50025559156bffc86
Svyatoslav Oprysko <opryskosv@gmail.com>
2018-11-01 21:38:38 +0200

    Started adding 'click-important'.

--- src/components/todo-list-item/TodoListItem.js
+++ src/components/todo-list-item/TodoListItem.js
@@ -4,40 +4,40 @@ import "./TodoListItem.css"
 export default class TodoListItem extends React.Component {
 
     state = {
-      done: false
+        done: false,
+        important: false
     };
 
     onLabelClicked = () => {
         this.setState({
-           done: true
+            done: true
         });
     };
 
     render() {
-        const {label, important = false} = this.props;
-        const {done} = this.state;
-
-        const style = {
-            color: important ? 'orange' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        const {label} = this.props;
+        const {done, important} = this.state;
 
         let classNames = "todo-list-item";
-        if(done) {
+        if (done) {
             classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
         }
 
         return (
             <span className={classNames}>
                 <span
                     className="todo-list-item-label"
-                    style={style}
                     onClick={this.onLabelClicked}>
                   {label}
                 </span>
 
                 <button type="button"
-                        className="btn btn-outline-success btn-sm float-right">
+                        className="btn btn-outline-success btn-sm float-right"
+                        onClick={this.onMarkImportant}>
                   <i className="fa fa-exclamation"/>
                 </button>
 
