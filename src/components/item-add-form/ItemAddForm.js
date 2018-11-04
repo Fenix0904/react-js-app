e61a00f5b459285b39148eabefdb2ef736074a1f
Svyatoslav Oprysko <opryskosv@gmail.com>
2018-11-04 14:52:36 +0200

    Added input for adding new items.

--- src/components/item-add-form/ItemAddForm.js
+++ src/components/item-add-form/ItemAddForm.js
@@ -4,18 +4,35 @@ import "./ItemAddForm.css";
 export default class ItemAddForm extends React.Component {
 
     state = {
-      label: ""
+        label: ""
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onAddingNewItem = (e) => {
+        e.preventDefault();
+        this.props.onAddItem(this.state.label);
+        this.setState({
+            label: ""
+        });
     };
 
     render() {
-        const {onAddItem} = this.props;
-        return(
-            <form className="item-add-form d-flex">
+        return (
+            <form
+                className="item-add-form d-flex"
+                onSubmit={this.onAddingNewItem}>
                 <input type="text"
+                       value={this.state.label}
+                       onChange={this.onLabelChange}
                        className="form-control"
                        placeholder="What needs to be done"/>
-                <button className="btn btn-outline-dark"
-                    onClick={() => onAddItem("New item")}>Add new item</button>
+                <button className="btn btn-outline-dark">Add item
+                </button>
             </form>
         );
     }
